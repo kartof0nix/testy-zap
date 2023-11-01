aa158b0908b31a3b4e0c24b844701f099f5c72f9
Karol Bonat <karol.bonatkarol@gmail.com>
2023-11-01 20:29:48 +0100

    Dodaj wyjsćia i testerkę.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # testy-zap
-Testy do zadania zapobiegliwy student z OI
+Testerka do zadania zapobiegliwy student z OI31.
+
+# Użytkowanie
+W folderze znajdziesz 4 pliki:
+* testuj.cpp : Generatorka i testerka.
+* zap1.out : 10^7 odpowiedzi dla n<=16 a,b<=32
+* zap2.out : 3.5*10^5 odpowiedzi dla n<=3000 a,b<=10^9
+* zap3.out : 1000 odpowiedzi dla n<=500000 a,b <= 10^9
+
+Użytkowanie:
+1. Do pliku testuj.cpp wklej swoją wzorcówkę.
+    1. Zmień nazwę ``main`` na ``solve`` i zamiast cin przepisz dane z argumentu typu ``pair<int, vector<pair<int, int>>>``
+    2. Zmień wypisywanie na zwracanie wyniku (tylko 1 wiersz).
+2. Ustaw zmienną path na ścieżkę do odpowiedniego wyjścia
+3. Odkomentuj odpowiednie dla testów MAXN, MAXT i TESTS
+4. Skompiluj i wykonaj testuj.cpp.
+
+Porady:
+1. Nie czyszczenie tablic może prowadzić do blędnych wyników. Polecam je dodać
+2. Polecam zmienić w swoim programie rozmiar tablic na odpowiednio mniejszy. Potrafi to mocno przyśpieszyć testowanie ( szczególnie jeśli czyścisz całą tablicę)
+
+
+Testuj.cpp jest wypełnione przykładowym rozwiązaniem.
+
+FAQ:
+> Mam runtime error. Jak wypisać test? 
+
+Zmień w testerce początek pętli na ostatni test który wypisał że przeszedł(np. int i = 31000). Odkomentuj wypisywanie testu przed wykonaniem go.
+
+> Testerka nie działa, gdzie to zgłosić
+
+Jeśli nie działa testerka lub generatorka, zgłoś to przed github zakładkę 'Issues'. Jeśli uważasz że dla danego testu wygenerowałem złą odpowiedź, napisz o tym na forum. Uwaga: jeśli pierwszy test jest ŹLE, to prawdopodobnie wina generatorki, więc zgłoś to przez github.
+
---
+++ testuj.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+// --- USTAWIENIA ---
+
+constexpr int I;
+string path = "./zap1.out";
+// Odkomentuj sekcję dla odpowiedniego pliku
+// -> zap1.out
+constexpr int TESTS = 10000000, INTERVAL=1000;
+constexpr int MN=16, MAXT=32;
+// -> zap2.out
+// constexpr int TESTS = 350000, INTERVAL=100;
+// constexpr int MN=3000, MAXT=1000000000;
+// -> zap3.out
+// constexpr int TESTS = 1000, INTERVAL=1;
+// constexpr int MN= 500000, MAXT=1000000000;
+
+
+// --------- WZORCÓWKA -----------
+
+int tab[MN]; // Możesz użyć zmiennych testerki
+
+int solve(pair<int, vector<pair<int, int>>> wej){
+	for(int i = 1; i < 5; i++)
+		tab[i]=0;
+	int n = wej.first;
+	return n;
+}
+
+// --------- GENERATORKA ---------
+#include <ctime>  
+#include <iostream> 
+#include <random> 
+  
+mt19937 mt;  
+
+int r(int a, int b){
+    return mt() %(b-a+1) + a;
+}
+
+
+
+pair<int, vector<pair<int, int>>> gen(int seed){ 
+    mt.seed(seed);
+
+    int n = r(2, MN);
+    vector<pair<int, int>> res;
+    for(int i = 1; i <= n; i++){
+        int a = r(1, MAXT-1);
+        int b = r(a+1, MAXT);
+        // cout << a << ' ' << b << '\n';
+        res.push_back({a, b});
+    }
+    return {n, res};
+}
+
+// -------- TESTERKA ----------
+
+void print_test(pair<int, vector<pair<int, int>>> t){
+    // cout <
+    cout << t.first << '\n';
+    for(auto i : t.second){
+        cout << i.first << ' ' << i.second << '\n';
+    }
+}
+
+ifstream odp;
+int main(){
+    odp.open(path);
+    for(int i = 1; i <= TESTS; i++){
+        auto in = gen(i);
+        int cur_res=-1;
+
+		//Jeśli masz RE, odkomentuj tą sekcję żeby wypisywać test przed wykonaniem
+		// cout << "TEST " << i << "\n";
+		// cout << "Wejscie:\n";
+		// print_test(in);
+
+		cur_res = solve(in);
+		
+		int ans=-1;
+		odp >> ans;
+        if(ans != cur_res){
+			cout << "TEST " << i << " : WA\n";
+			cout << "Wejscie:\n";
+            print_test(in);
+			cout << "Wyjscie:\n" << cur_res << '\n' << "Odpowiedź:\n" << ans << '\n';
+            
+            break;
+        }
+
+        if(i%1000 == 0){
+            cout << "TESTY do " << i << " OK\n";
+        }
+    }
+}
